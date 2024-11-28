542b7872350a2feffb233cd6d7a9ba82bad67254
jiangplus <jiang.plus.times@gmail.com>
2024-11-28 20:05:58 +0800

    fix

--- app/controllers/api/profile_controller.rb
+++ app/controllers/api/profile_controller.rb
@@ -396,5 +396,8 @@ class Api::ProfileController < ApiController
 
   def profile_params
     params.require(:profile).permit(:image_url, :nickname, :about, :social_links, :location)
+    if params[:profile][:social_links]
+      params[:profile][:social_links].permit!
+    end
   end
 end
