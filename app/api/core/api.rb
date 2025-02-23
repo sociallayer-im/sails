021350d5cbd1e552ae98aec8815caf862240f9e5
jiangplus <jiang.plus.times@gmail.com>
2025-02-24 03:17:56 +0800

    add grape api for event list

--- app/api/core/api.rb
+++ app/api/core/api.rb
@@ -38,6 +38,14 @@ module Core
     expose :event_roles, using: Core::EventRoleEntity
     expose :tickets, using: Core::TicketEntity, if: { type: :full }
     expose :custom_form, using: Core::CustomFormEntity, if: { type: :full }
+    expose :is_starred, as: :is_starred, if: lambda { |instance, options|
+      options[:with_stars] == true && options[:stars].find {|x| x == instance.id }.present?
+    }
+  end
+
+  class PopupCityEntity < Grape::Entity
+    expose :id, :title, :location, :start_date, :end_date, :group_tags, :website, :image_url
+    expose :group, using: Core::GroupEntity
   end
 
 
@@ -114,9 +122,14 @@ module Core
       current_profile
     end
 
+    get "event/themes" do
+      themes = Event.where(group_id: params[:group_id]).distinct(:theme).pluck(:theme)
+      { themes: themes }
+    end
+
     get "event/get" do
       @event = Event.includes(:owner).find(params[:id])
-      present @event, with: Core::EventEntity
+      present :event, @event, with: Core::EventEntity
     end
 
     get "event/discover" do
@@ -125,29 +138,204 @@ module Core
       @popups = PopupCity.includes(:group).where.not("group_tags @> ARRAY[?]::varchar[]", [":cnx", ":bkk"]).order(start_date: :desc)
       @groups = Group.includes(:owner).where("group_tags @> ARRAY[?]::varchar[]", [":top"]).order(handle: :desc)
 
+      present :events, @events, with: Core::EventEntity
+      present :featured_popups, @featured_popups, with: Core::PopupCityEntity
+      present :popups, @popups, with: Core::PopupCityEntity
+      present :groups, @groups, with: Core::GroupEntity
     end
 
-    # get "event/get", to: "event#get"
-    # get "event/discover", to: "event#discover"
-    # get "event/list", to: "event#list"
-    # get "event/themes_list", to: "event#themes_list"
-    # get "event/my_stars", to: "event#my_stars"
-    # get "event/list_for_calendar", to: "event#list"
-    # get "event/private_list", to: "event#private_list"
-    # get "event/private_track_list", to: "event#private_track_list"
-    # get "event/my_event_list", to: "event#my_event_list"
-    # get "event/starred_event_list", to: "event#starred_event_list"
-    # get "event/created_by_me", to: "event#created_by_me"
-    # get "event/latest_changed", to: "event#latest_changed"
-    # get "event/pending_approval_list", to: "event#pending_approval_list"
+    get "event/my_starred" do
+      profile = current_profile!
+      @stars = Comment.where(profile_id: profile.id, comment_type: "star", item_type: "Event").pluck(:item_id)
+      @events = Event.where(id: @stars).order(start_time: :desc)
+      @with_stars = true
 
+      limit = params[:limit] ? params[:limit].to_i : 40
+      limit = 1000 if limit > 1000
+      @events = @events.limit(limit)
+      present :events, @events, with: Core::EventEntity
+    end
 
-  # def get
-  #   @event = Event.includes(:owner).find(params[:id])
-  #   render template: "api/event/show"
-  # end
+    get "event/my_attending" do
+      profile = current_profile!
 
+      @events = Event.joins(:participants).where(participants: { profile_id: profile.id, status: ["attending", "checked"] })
+      if params[:collection] == "upcoming"
+        @events = @events.where("end_time >= ?", DateTime.now)
+      elsif params[:collection] == "past"
+        @events = @events.where("end_time < ?", DateTime.now)
+      end
+      @events = @events.order(start_time: :asc)
 
+      limit = params[:limit] ? params[:limit].to_i : 40
+      limit = 1000 if limit > 1000
+      @pagy, @events = pagy(@events, limit: limit)
+      present :events, @events, with: Core::EventEntity
+    end
+
+    get "event/my_created" do
+      @events = Event.where(owner: current_profile).order(start_time: :desc)
+
+      limit = params[:limit] ? params[:limit].to_i : 40
+      limit = 1000 if limit > 1000
+      @pagy, @events = pagy(@events, limit: limit)
+      present :events, @events, with: Core::EventEntity
+    end
+
+    get "event/my_private" do
+      profile = current_profile!
+      group_ids = Membership.where(profile_id: profile.id, role: ["owner", "manager"]).pluck(:target_id)
+
+      @events = Event.where(owner: profile)
+      .or(Event.where(group_id: group_ids))
+      .or(Event.joins(:event_roles).where(event_roles: { item_type: "Profile", item_id: profile.id }))
+
+      @events = @events.where(status: "published").where(display: ["hidden"]).order(start_time: :desc)
+
+      limit = params[:limit] ? params[:limit].to_i : 40
+      limit = 1000 if limit > 1000
+      @pagy, @events = pagy(@events, limit: limit)
+      present :events, @events, with: Core::EventEntity
+    end
+
+    get "event/my_private_track" do
+      profile = current_profile!
+      group_id = params[:group_id]
+      group = Group.find(group_id)
+      @group = group
+      my_tracks = Track.where(group_id: group_id, kind: "public").ids + TrackRole.where(group_id: group_id, profile_id: profile.id).pluck(:track_id)
+      my_tracks << nil
+      @events = Event.where(group_id: group_id).where(status: ["open", "published", "closed"]).where(display: ["normal", "pinned", "public"])
+      @events = @events.where(track_id: my_tracks)
+
+      if params[:start_date].present? && params[:end_date].present?
+        start_time = Date.parse(params[:start_date]).in_time_zone(@timezone).at_beginning_of_day
+        end_time = Date.parse(params[:end_date]).in_time_zone(@timezone).at_end_of_day
+        @events = @events.where("start_time >= ?", start_time).where("end_time <= ?", end_time)
+        @events = @events.order(start_time: :asc)
+      elsif params["collection"] == "upcoming"
+        @events = @events.where("end_time >= ?", DateTime.now)
+        @events = @events.order(start_time: :asc)
+      elsif params["collection"] == "past"
+        @events = @events.where("end_time < ?", DateTime.now)
+        @events = @events.order(start_time: :desc)
+      else
+        @events = @events.order(start_time: :asc)
+      end
+
+      limit = params[:limit] || 40
+      limit = 500 if limit > 500
+      @pagy, @events = pagy(@events, limit: limit)
+      present :events, @events, with: Core::EventEntity
+    end
+
+    get "event/pending" do
+      group_ids = Membership.where(profile_id: current_profile.id, role: ["owner", "manager"]).pluck(:target_id)
+      @events = Event.includes(:owner, :event_roles).where(status: "pending").where(group_id: group_ids).order(created_at: :desc)
+      present :events, @events, with: Core::EventEntity
+    end
+
+    get "event/list" do
+      @group = Group.where("id = ? OR handle = ?", params[:group_id], params[:group_id]).first
+      group_id = @group.id
+
+      if @group.status == "freezed"
+        error!({ result: 'error', message: "group is freezed" }, 400)
+      end
+
+      auth_profile = current_profile
+      if auth_profile
+        if @group.is_manager(auth_profile.id)
+          pub_tracks = Track.where(group_id: group_id).ids
+          pub_tracks << nil
+        elsif @group.group_union.present?
+          managing_groups = Membership.where(profile_id: auth_profile.id, role: ["owner", "manager", "operator"], id: @group.group_union).ids
+          pub_tracks = Track.where(group_id: managing_groups).ids + Track.where(group_id: @group.group_union, kind: "public").ids + TrackRole.where(group_id: @group.group_union, profile_id: auth_profile.id).pluck(:track_id)
+          pub_tracks = pub_tracks.compact
+          pub_tracks << nil
+        else
+          pub_tracks = Track.where(group_id: group_id, kind: "public").ids + TrackRole.where(group_id: group_id, profile_id: auth_profile.id).pluck(:track_id)
+          pub_tracks << nil
+        end
+      else
+        pub_tracks = Track.where(group_id: group_id, kind: "public").ids
+        pub_tracks << nil
+      end
+
+      if params[:track_id] && !pub_tracks.include?(params[:track_id].to_i)
+        error!({ result: 'error', message: "track not found" }, 400)
+      end
+
+      event_group_ids = @group.group_union.present? ? [@group.id] + @group.group_union : [@group.id]
+
+      @timezone = @group.timezone || params[:timezone] || 'UTC'
+      @events = Event.includes(:group, :venue, :owner, :event_roles).where(status: ["open", "published", "closed"]).where(group_id: event_group_ids)
+      if @group.can_view_event == "member"
+        if (auth_profile.blank? || !@group.is_member(auth_profile.id))
+          @events = @events.where("events.tags @> ARRAY[?]::varchar[]", ["public"])
+        end
+      elsif params[:private_event].present? && auth_profile && @group.is_manager(auth_profile.id)
+        @events = @events.where(display: "private")
+      elsif params[:private_event].present?
+        @events = @events.where(display: "none")
+      else
+        @events = @events.where(display: ["normal", "pinned", "public"])
+      end
+
+      if params[:track_id]
+        @events = @events.where(track_id: params[:track_id])
+      else
+        @events = @events.where(track_id: pub_tracks)
+      end
+      if params[:tags]
+        tags = params[:tags].split(",")
+        @events = @events.where("events.tags && ARRAY[:options]::varchar[]", options: tags)
+        # @events = @events.where("tags @> ARRAY[?]::varchar[]", tags)
+      end
+      if params[:search_title]
+        @events = @events.where("events.title like ?", "%#{params[:search_title]}%")
+      end
+      if params[:venue_id]
+        @events = @events.where(venue_id: params[:venue_id])
+      end
+      if params[:theme]
+        @events = @events.where(theme: params[:theme])
+      end
+
+      if params[:skip_multiday].present?
+        @events =  @events.where("end_time - start_time <= interval '1 day'")
+      end
+      if params[:skip_recurring].present?
+        @events = @events.where(recurring_id: nil)
+      end
+
+      if params[:start_date].present? && params[:end_date].present?
+        start_time = Date.parse(params[:start_date]).in_time_zone(@timezone).at_beginning_of_day
+        end_time = Date.parse(params[:end_date]).in_time_zone(@timezone).at_end_of_day
+        @events = @events.where("start_time <= ? AND end_time >= ?", end_time, start_time)
+      elsif params[:start_time].present? && params[:end_time].present?
+        start_time = params[:start_time]
+        end_time = params[:end_time]
+        @events = @events.where("start_time <= ? AND end_time >= ?", end_time, start_time)
+      elsif params[:collection] == "upcoming"
+        @events = @events.where("end_time >= ?", DateTime.now)
+      elsif params[:collection] == "past"
+        @events = @events.where("end_time < ?", DateTime.now)
+      elsif params[:collection] == "pinned"
+        @events = @events.where(pinned: true)
+      end
+      @events = @events.order(start_time: :asc)
+
+      if auth_profile && params[:with_stars]
+        @with_stars = true
+        @stars = Comment.where(item_id: @events.ids, profile_id: auth_profile.id, comment_type: "star", item_type: "Event").pluck(:item_id)
+      end
+
+      limit = params[:limit] || 40
+      limit = 500 if limit > 500
+      @pagy, @events = pagy(@events, limit: limit)
+      present :events, @events, with: Core::EventEntity, with_stars: @with_stars, stars: @stars
+    end
 
   end
 end
--- test/integration/core/api_test.rb
+++ test/integration/core/api_test.rb
@@ -16,15 +16,35 @@ module Core
 
       assert_response :success
       assert_equal(profile.id, JSON.parse(@response.body)["id"])
-    end
+      end
 
-    test "GET /api/event/get returns event" do
-      event = events(:one)
-      get "/api/event/get", params: { id: event.id }, headers: { "Authorization" => "Bearer #{event.owner.gen_auth_token}" }
+      test "GET /api/event/get returns event" do
+        event = events(:one)
+        get "/api/event/get", params: { id: event.id }, headers: { "Authorization" => "Bearer #{event.owner.gen_auth_token}" }
 
-      assert_response :success
-      assert_equal(event.id, JSON.parse(@response.body)["id"])
-      p @response.body
-    end
+        assert_response :success
+        assert_equal(event.id, JSON.parse(@response.body)["event"]["id"])
+        # p @response.body
+      end
+
+      test "GET /api/event/discover returns featured events and popups" do
+        travel_to DateTime.new(2024, 2, 22, 10, 0, 0, "+00:00")
+        get "/api/event/discover"
+
+        assert_response :success
+        assert_equal(1, JSON.parse(@response.body)["events"].count)
+      end
+
+      test "GET /api/event/my_starred returns my starred events" do
+        travel_to DateTime.new(2024, 2, 22, 10, 0, 0, "+00:00")
+        profile = profiles(:one)
+        auth_token = profile.gen_auth_token
+        Comment.create(profile: profile, comment_type: "star", item_type: "Event", item_id: events(:one).id)
+
+        get "/api/event/my_starred", headers: { "Authorization" => "Bearer #{auth_token}" }
+
+        assert_response :success
+        assert_equal(1, JSON.parse(@response.body)["events"].count)
+      end
   end
 end
